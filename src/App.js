f040e25da62d69c30ba9375df37bf2b64c0c6e72
MichaeTav <m.tavera@hotmail.com>
2023-01-28 14:23:31 -0800

    Added navigation that scrolls to section of page when clicked

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import React from "react";
+import { useRef } from "react";
+
+import Navigationmenu from "./components/Navigationmenu";
+import Projects from "./sections/Projects";
+import Skills from "./sections/Skills";
+import About from "./sections/About";
+
 import "./App.css";
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  const projects = useRef(null);
+  const skills = useRef(null);
+  const about = useRef(null);
+  const refs = { projects: projects, skills: skills, about: about };
+
+  const scrollToSection = (elementRef) => {
+    window.scrollTo({
+      top: elementRef.current.offsetTop - 48,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div>
+      <Navigationmenu scrollToSection={scrollToSection} refs={refs} />
+      <Projects innerRef={projects} />
+      <Skills innerRef={skills} />
+      <About innerRef={about} />
+    </div>
+  );
+};
 
 export default App;

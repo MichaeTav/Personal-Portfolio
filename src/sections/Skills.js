b198796bc4946512388c727b1fe27ef40f079ca3
MichaeTav <m.tavera@hotmail.com>
2023-01-28 23:10:56 -0800

    Skills section redone

--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -1,35 +1,60 @@
 import React from "react";
 
-import { Stack } from "@mui/material";
-import { motion } from "framer-motion";
+import { Grid, Stack, Box, Paper, Container } from "@mui/material";
 
 import graphqlIcon from "../resources/graphql-icon.png";
 import javaIcon from "../resources/java-icon.png";
 import javaScriptIcon from "../resources/javascript-icon.png";
 import reactIcon from "../resources/react-icon.png";
 import springIcon from "../resources/spring-icon.png";
+import materialuiIcon from "../resources/materialui-icon.png";
+import githubIcon from "../resources/github-icon.png";
+import chromeIcon from "../resources/chrome-icon.png";
+import dockerIcon from "../resources/docker-icon.png";
+
+const languages = [javaIcon, javaScriptIcon, graphqlIcon];
+const frameworks = [reactIcon, springIcon, materialuiIcon];
+const tools = [githubIcon, chromeIcon, dockerIcon];
+
+const SkillsContainer = ({ title, icons }) => {
+  return (
+    <Stack spacing={2}>
+      <h1 className="sectionHeader">{title}</h1>
+      <Paper
+        sx={{
+          backgroundColor: "#252526",
+          maxWidth: "250px",
+          minWidth: "250px",
+          margin: "auto",
+        }}
+        elevation={10}
+      >
+        <Grid container>
+          {icons.map((icon) => (
+            <Grid item xs={6} justifyContent="center" alignItems="center">
+              <img src={icon} className="icon" />
+            </Grid>
+          ))}
+        </Grid>
+      </Paper>
+    </Stack>
+  );
+};
 
 const Skills = ({ innerRef }) => {
   const skills = [javaIcon, javaScriptIcon, reactIcon, graphqlIcon, springIcon];
 
   return (
     <div ref={innerRef} className="skills">
-      <h1 className="sectionHeader">SKILLS</h1>
-
-      <Stack spacing={10}>
-        {skills.map((icon) => (
-          <motion.div
-            initial={{ x: "-95vw" }}
-            viewport={{ once: true }}
-            whileInView={{
-              x: 0,
-              transition: { type: "spring", duration: 1.5, bounce: 0.3 },
-            }}
-            key={icon}
-          >
-            <img src={icon} className="icon" />
-          </motion.div>
-        ))}
+      <Stack
+        spacing={8}
+        direction={{ xs: "column", md: "row" }}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <SkillsContainer title={"LANGUAGES"} icons={languages} />
+        <SkillsContainer title={"FRAMEWORKS"} icons={frameworks} />
+        <SkillsContainer title={"TOOLS"} icons={tools} />
       </Stack>
     </div>
   );

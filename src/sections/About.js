63151953bb5e597ad16852ff3303b8ce2cafdee0
MichaeTav <m.tavera@hotmail.com>
2023-01-29 18:05:39 -0800

    Added about section

--- src/sections/About.js
+++ src/sections/About.js
@@ -1,5 +1,10 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Paper, Typography, Avatar } from "@mui/material";
 import React from "react";
+
+import profile from "../resources/profilepic.jpg";
+
+const description =
+  "My name is Michael Tavera and I am currently a computer science student at Cal State University San Marcos. I have a strong passion for computer science and have been working hard to improve my skills in the field. Prior to attending Cal State University, I completed my associate degree in Computer Science for Transfer at MiraCosta College, where I maintained a GPA of 3.89. I also have experience working as a STEM tutor at MiraCosta College, where I helped students with coursework in computer science, math, and physics. ";
 
 const About = ({ innerRef }) => {
   return (
@@ -8,6 +13,23 @@ const About = ({ innerRef }) => {
         {/* Pushes header to front of components avoiding animation from overlapping */}
         <div style={{ zIndex: 1 }}>
           <h1 className="sectionHeader">ABOUT</h1>
+          <Paper
+            sx={{
+              backgroundColor: "#252526",
+              maxWidth: "1000px",
+              minWidth: "250px",
+              margin: "auto",
+              padding: "40px",
+            }}
+            elevation={10}
+          >
+            <Stack justifyContent="center" alignItems="center" spacing={8}>
+              <Avatar src={profile} sx={{ width: 100, height: 100 }} />
+              <Typography variant="h6" color="#fff">
+                {description}
+              </Typography>
+            </Stack>
+          </Paper>
         </div>
       </Stack>
     </div>

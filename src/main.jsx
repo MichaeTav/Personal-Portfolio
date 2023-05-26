85e9aad2da4d4121a229fd957eb63fef0c8091e3
MichaeTav <m.tavera@hotmail.com>
2023-05-25 17:47:16 -0700

    new version :) !!!!

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
+import Project from "./pages/Project.jsx";
+import Projects from "./pages/Projects.jsx";
+import Experience from "./pages/Experience.jsx";
+import Blog from "./pages/Blog.jsx";
+import Home from "./pages/Home.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/projects",
+        element: <Projects />,
+      },
+      {
+        path: "/experience",
+        element: <Experience />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
+      },
+    ],
+  },
+  {
+    path: "/:projectName",
+    element: <Project />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </React.StrictMode>
+);
